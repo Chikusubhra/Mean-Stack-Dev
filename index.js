c71865c06a2b77c583b190fe87c5424b3871279d
Subhrajit Das <166319237+Chikusubhra@users.noreply.github.com>
2026-06-06 18:11:23 +0530

    trying javascript from the basics

---
+++ index.js
@@ -0,0 +1,114 @@
+// console.log("hello")
+// var x=10
+// var y=20
+// var add=x+y
+// var sub=x-y
+// var mul=x*y
+// var div=x/y
+// console.log(`add= ${add}`)
+// console.log(`sub= ${sub}`)
+// console.log(`mul= ${mul}`)
+// console.log(`div= ${div}`)
+// let s1="chiku"
+// let s2="piku"
+// console.log(`s1+s2= ${s1+s2}`)
+// let x=10
+// let y=20
+// let res=x+y
+// console.log(res)
+// var x=10
+// var y=20
+// if(x<y){
+//     var z=x+y
+//     console.log("z= ",z)
+// }
+// console.log("z= ",z)
+// var x=10
+// var y=20
+// if(x<y){
+//     let z=x+y
+//     console.log("z= ",z)
+// }
+// console.log("z= ",z)
+// const x=10
+// x=x+5
+// console.log("x= ",x)
+// let x=20
+// let y=30
+// console.log(x%y)
+// console.log(x++)
+// console.log(++x)
+// console.log(--x)
+// console.log(x--)
+// console.log(y--)
+// console.log(y++)
+// console.log(--y)
+// console.log(++y)
+// var season="rainy"
+// if(season=="rainy"){
+//     console.log("must to carry umbrella")
+// }else{
+//     console.log("no need to carry umberlla")
+// }
+// var x=10
+// var y=5
+// var z=(x>y)?x:y //ternary operator
+// console.log("z= ",z)
+// var x=10
+// let sum=0
+// for(let i=0;i<x;i++){
+//     console.log(`i- ${i}`)
+//     sum=sum+i
+// }
+// console.log(`sum= ${sum}`)
+// var x=10
+// var i=0
+// var sum=0
+// while(i<x){
+//     console.log("i= ",i)
+//     sum=sum+i
+//     // var sum=sum+i
+//     i++
+// }
+// console.log(`sum= ${sum}`)
+// var i=0
+// do{
+//     console.log("hello ",i)
+//     i++
+// }while(i<10)
+// for(let i=0;i<10;i++){
+//     if(i==4){
+//         // break
+//         continue
+//     }
+//     console.log(i)
+// }
+// let signal="red"
+// var msg=""
+// switch(signal){
+//     case "green":
+//         msg="go safely"
+//         console.log("green color ",msg)
+//         break
+//     case "yellow":
+//         msg="ready to go"
+//         console.log("yellow color ",msg)
+//         break
+//     case "red":
+//         msg="stop immediately"
+//         console.log("red color ",msg)
+//         break
+//     default:
+//         msg="there is no color"
+//         console.log("don't wait ",msg);        
+// }
+// function sports(){
+//     console.log("IPL cricket is very popular in India")
+//     console.log("IPL cricket win by RCB")
+// }
+// sports()
+// const add=(a, b)=>{
+//     console.log("I'm very fast")
+//     console.log(a+b)
+// }
+// add(10,20)
